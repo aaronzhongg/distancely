b440d312275614ce08c0e95740857faa2b845c0b
Aaron Zhong <aaronzhong96@gmail.com>
2021-02-17 21:23:05 +1300

    Restructure project - put all backend code in server folder

--- src/server/tests/UnitTests/Tests/CalculateDistanceToTest.cs
+++ src/server/tests/UnitTests/Tests/CalculateDistanceToTest.cs
@@ -10,12 +10,14 @@ namespace UnitTests.Tests
     public class CalculateTravelTimesTest
     {
         [Theory, UseCaseTest]
-        public async Task Given_valid_from_and_to_address_When_calculating_distance_to_Then_should_return_distance_at_current_time(CalculateDistanceToUseCase sut)
+        public async Task Given_valid_from_and_to_address_When_calculating_distance_to_Then_should_return_distance_at_current_time(
+            CalculateDistanceToPresenter presenter,
+            CalculateDistanceToUseCase sut
+            )
         {
             // Given
             var fromAddress = "96 Holly Street, Avondale, New Zealand";
             var toAddress = "1 Nelson Street, Auckland, New Zealand";
-            CalculateDistanceToPresenter presenter = new CalculateDistanceToPresenter(); // todo: PreCondition
             sut.SetOutputPort(presenter);
 
             // When
