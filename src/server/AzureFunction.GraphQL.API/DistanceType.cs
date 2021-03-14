fe857f8524ccaaca583deb9b820cdd2224e81544
Aaron Zhong <aaronzhong96@gmail.com>
2021-03-14 23:40:31 +1300

    Add AzureFunction GraphQL API

---
+++ src/server/AzureFunction.GraphQL.API/DistanceType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Domain;
+using GraphQL.Types;
+using GraphQL.Utilities;
+
+namespace AzureFunction.GraphQL.API
+{
+    // todo: break this up. should it be in another GraphQL project to decouple the types, query and schemas with az funcs?
+    // todo: how will resolvers use the application use cases to resolve a distance object based on inputs?
+    public class DistanceType : ObjectGraphType<Distance>
+    {
+        public DistanceType()
+        {
+            Field(x => x.DistanceMeters).Description("Distance in meters.");
+            Field(x => x.TravelTime).Description("Travel time in seconds.");
+        }
+    }
+
+    public class DistanceQuery : ObjectGraphType
+    {
+        public DistanceQuery()
+        {
+            Field<DistanceType>(
+              "distance",
+              resolve: context => new Distance { DistanceMeters= 1, TravelTime = 1 }
+            );
+        }
+    }
+
+    public class DistancelySchema : Schema
+    {
+        public DistancelySchema(IServiceProvider provider)
+            : base(provider)
+        {
+            Query = provider.GetRequiredService<DistanceQuery>();
+            // Mutation = provider.GetRequiredService<StarWarsMutation>();
+        }
+    }
+}
---
+++ src/server/AzureFunction.GraphQL.API/Startup.cs
@@ -0,0 +1,25 @@
+﻿using AzureFunction.GraphQL.API;
+using GraphQL;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Server;
+using GraphQL.Types;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace AzureFunction.GraphQL.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<DistanceQuery>();
+            builder.Services.AddSingleton<DistanceType>();
+            builder.Services.AddSingleton<ISchema, DistancelySchema>();
+
+            builder.Services.AddSingleton<IDocumentExecuter>(sp => new DocumentExecuter());
+            builder.Services.AddSingleton<IDocumentWriter>(new DocumentWriter());
+            builder.Services.AddGraphQL();
+        }
+    }
+}
