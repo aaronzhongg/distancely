901ae9ecc0581d2b83bd398914ace8a465d24593
Aaron Zhong <aaronzhong96@gmail.com>
2021-03-18 21:52:08 +1300

    Add apollo client and graphql to web app

--- src/webapp/src/index.tsx
+++ src/webapp/src/index.tsx
@@ -4,6 +4,25 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "typeface-inter";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:7071/api/GraphQL", // todo: put in settings file
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetDistance {
+        distance {
+          distanceMeters
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
